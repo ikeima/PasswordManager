2f08088158a760670649ce86df17439ca8c2a542
ikeima <ikeima@yandex.ru>
2024-09-03 20:06:27 +0500

    Add PasswordFeedbackService (not implemented)

---
+++ Services/IPasswordFeedbackService.cs
@@ -0,0 +1,10 @@
+﻿namespace PasswordManager.Services
+{
+    public interface IPasswordFeedbackService
+    {
+        Task<int> GetCountAsync();
+        Task<string> GetPasswordAsync();
+        Task<string> GetHashByFeedbackAsync();
+        Task<bool> AddNewCombinationAsync();
+    }
+}
---
+++ Services/PasswordFeedbackService.cs
@@ -0,0 +1,25 @@
+﻿namespace PasswordManager.Services
+{
+    public class PasswordFeedbackService : IPasswordFeedbackService
+    {
+        public Task<bool> AddNewCombinationAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetCountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetHashByFeedbackAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPasswordAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
